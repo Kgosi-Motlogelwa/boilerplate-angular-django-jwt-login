6806dc9fc3dcc85bb76260fb536a83549019f22f
Kgosi Motlogelwa <komotlogelwa@gmail.com>
2020-10-10 16:53:14 +0200

    change database and downloaded necessary files

--- api/api/settings.py
+++ api/api/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'boilerplate-angular-django-jwt-login',
+        'USER': 'postgres',
+        'PASSWORD': 'Mark8Prost',
+        'HOST': 'localhost',
     }
 }
 
